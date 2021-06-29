9cab3158da7878c2276016164612983670432e1b
Aryan Behnam <aryan.behnam@hansrobot.de>
2021-06-29 13:34:43 +0200

    feat: Layout, Header and Footer

---
+++ pages/about/index.js
@@ -0,0 +1,15 @@
+import Layout from '../../components/Layout'
+
+export default function AboutPage() {
+    return (
+        <Layout title="About DJ Events">
+            <h1>About</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita velit, doloremque
+                animi facere sint delectus. Cumque rerum cupiditate quidem. Qui doloremque dolores
+                reiciendis ipsum repellat molestias maiores culpa voluptatem repellendus.
+            </p>
+        </Layout>
+    )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout'
+
+export default function EventsPage() {
+    return (
+        <Layout>
+            <div>
+                <h1>Event Page</h1>
+            </div>
+        </Layout>
+    )
+}
